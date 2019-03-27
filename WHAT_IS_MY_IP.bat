005e2696743aa130a444f7f1e96ced057d12d922
Karolis <karolis.uzkuraitis@gmail.com>
2019-03-27 18:17:34 +0200

    2019-03-27

--- WHAT_IS_MY_IP.bat
+++ WHAT_IS_MY_IP.bat
@@ -1,6 +1,6 @@
 @echo off
 echo -----------------------------------
-ipconfig | findstr "IPv4 Address" | findstr "192.168.0"
+ipconfig | findstr "IPv4 Address" | findstr "192.168."
 
 echo Junkis i kazkuri is situ visuje pateiktu ip.
 pause
